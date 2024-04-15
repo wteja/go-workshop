d7bca9887cdab3cc0fb1e06e6afebfbca9dfda1d
Weerayut Teja <teja.weerayut@gmail.com>
2024-04-15 23:18:20 +0700

    Adding generic

---
+++ tutorial5/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
+	var s V
+	for _, v := range m {
+		s += v
+	}
+	return s
+}
+
+func main() {
+	ints := map[string]int64{
+		"first":  34,
+		"second": 12,
+	}
+	floats := map[string]float64{
+		"first":  35.98,
+		"second": 26.99,
+	}
+
+	fmt.Printf("Non-Generic Sums: %v and %v\n", SumIntsOrFloats(ints), SumIntsOrFloats(floats))
+}
